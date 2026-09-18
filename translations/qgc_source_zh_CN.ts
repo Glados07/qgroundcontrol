510b85cfc77d9b530e39a1fd01868d72f7fee3ac
glados <happinesstrigger@163.com>
2026-09-18 14:15:11 +0800

    update (CEB-UI): 修改CEB配置中文翻译

--- translations/qgc_source_zh_CN.ts
+++ translations/qgc_source_zh_CN.ts
@@ -15389,17 +15389,17 @@ Do you want to set the Basic ID?</source>
     <message>
       <location filename="../src/AutoPilotPlugins/PX4/SafetyComponent.qml" line="385"/>
       <source>Normal braking distance:</source>
-      <translation>常规刹车距离：</translation>
+      <translation>常规触发距：</translation>
     </message>
     <message>
       <location filename="../src/AutoPilotPlugins/PX4/SafetyComponent.qml" line="403"/>
       <source>Mission braking distance:</source>
-      <translation>任务模式刹车距离：</translation>
+      <translation>任务模式触发距：</translation>
     </message>
     <message>
       <location filename="../src/AutoPilotPlugins/PX4/SafetyComponent.qml" line="419"/>
       <source>Cooldown margin:</source>
-      <translation>冷却解除余量：</translation>
+      <translation>冷却域宽度：</translation>
     </message>
     <message>
       <location filename="../src/AutoPilotPlugins/PX4/SafetyComponent.qml" line="435"/>
@@ -15411,7 +15411,7 @@ Do you want to set the Basic ID?</source>
       <source>Braking adds a speed-dependent stopping distance.
 Reactivation requires all monitored distances to stay outside the base distance plus the cooldown margin.</source>
       <translation>实际触发距离包含随接近速度变化的制动距离。
-所有监测方向的障碍距离持续达到基础距离与冷却解除余量之和后，才会恢复刹车触发。</translation>
+所有监测方向的障碍距离持续达到基础距离与冷却域宽度之和后，才会恢复刹车触发。</translation>
     </message>
     <message>
       <location filename="../src/AutoPilotPlugins/PX4/SafetyComponent.qml" line="459"/>
